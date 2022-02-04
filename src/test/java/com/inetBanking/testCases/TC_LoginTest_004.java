15ee392c1c4254494d268b4a36eb63b087b5ff16
kishore457 <kishoresmashes@gmail.com>
2022-02-04 13:32:15 +0530

    New test case TC004 added

---
+++ src/test/java/com/inetBanking/testCases/TC_LoginTest_004.java
@@ -0,0 +1,41 @@
+package com.inetBanking.testCases;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.inetBanking.pageObjects.LoginPage;
+
+public class TC_LoginTest_004 extends BaseClass{
+
+
+	@Test
+	public void loginTest() throws IOException, InterruptedException {
+		
+		//Interrupted Exception for Thread.sleep(2000);
+		
+		logger.info("URL is opened"); // this is logger variable created in base class.
+		LoginPage lp = new LoginPage(driver);
+		lp.setUserName(username);
+		logger.info("Enter username");
+		lp.setPassword(password);
+		logger.info("Enter password");
+		lp.clickLogin();
+		Thread.sleep(2000);
+		if(driver.getTitle().equals("Swag Lab")) {
+			Assert.assertTrue(true);
+		}else
+		{
+			captureScreen(driver,"loginTest");
+			Assert.assertTrue(false);
+			logger.info("TestFailed");
+		}
+		
+		driver.findElement(By.xpath("//div[contains(text(),\"Sauce Labs Backpack\")]")).isDisplayed();
+		
+	}
+
+	
+}
